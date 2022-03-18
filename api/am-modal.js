7c0663ce7d1e9fda3990cda8af5df5fb5dd3458e
Shawn <singlex@foxmail.com>
2022-03-18 14:59:18 +0800

    update icons to 6.1.0 removed some useless things

--- api/am-modal.js
+++ api/am-modal.js
@@ -1,7 +1,7 @@
-$(document).ready(function(e) {
+$(document).ready(function (e) {
 
   resizeDiv();
-  window.onresize = function(event) {
+  window.onresize = function (event) {
     resizeDiv();
   }
 
@@ -11,31 +11,32 @@ $(document).ready(function(e) {
     $('.am-modal').css('height', vph + "px");
   }
 
-  $('.am-modal-trigger').on('click', function() {
+  $('.am-modal-trigger').on('click', function () {
+    // bx - rotation
     var target = $(this).data('am-target');
-    $(target).css('display','block');
-    $(target).addClass('zoomIn').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
-    		$(target).removeClass('zoomIn');
+    $(target).css('display', 'block');
+    $(target).addClass('zoomIn').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function () {
+      $(target).removeClass('zoomIn');
     });
   });
-	
-  $('.am-close').on('click', function() {
-	  
-	var selector = $(this);  
-    selector.parent().parent().addClass('zoomOut').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
-    	selector.parent().parent().removeClass('zoomOut');
-        selector.parent().parent().css('display','none');
+
+  $('.am-close').on('click', function () {
+
+    var selector = $(this);
+    selector.parent().parent().addClass('zoomOut').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function () {
+      selector.parent().parent().removeClass('zoomOut');
+      selector.parent().parent().css('display', 'none');
     });
-  	
+
   });
-  
-  $(document).keyup(function(e) {
-		if (e.keyCode == 27) { // escape key maps to keycode `27`
-			$('.am-modal').addClass('zoomOut').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
-				$('.am-modal').removeClass('zoomOut');
-				$('.am-modal').css('display','none');
-			});
-		}
-	});
-  
+
+  $(document).keyup(function (e) {
+    if (e.keyCode == 27) { // escape key maps to keycode `27`
+      $('.am-modal').addClass('zoomOut').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function () {
+        $('.am-modal').removeClass('zoomOut');
+        $('.am-modal').css('display', 'none');
+      });
+    }
+  });
+
 });
